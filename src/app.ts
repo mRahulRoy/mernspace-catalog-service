697465f5bae7d40715225751edd43a3d7b93fc1d
rahul roy <rahulroy177602@gmail.com>
2024-03-26 14:25:39 +0530

    add authentication for create category

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express, { Request, Response } from "express";
 import config from "config";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
+import cookieParser from "cookie-parser";
+import authenticate from "./common/middlewares/authenticate";
+
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Hii from catalog service",
         port: config.get("server.port"),
     });
 });
-app.use("/categories", categoryRouter);
+app.use("/categories",authenticate,categoryRouter);
 app.use(globalErrorHandler);
 
 export default app;
